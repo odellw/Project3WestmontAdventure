4fac5f7d3f41e81e2944b1fd17b41d5dd9f85883
odellw <walker.odell@gmail.com>
2018-11-27 21:48:56 -0800

    fully functioning CRUD app with adventure as our model. Good starting point. Still having weird bugs that would be nice to deal with

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,6 +1,50 @@
 class AdventuresController < ApplicationController
   def index
-    @adventure = Adventure.all
+    @adventures = Adventure.all
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+      @adventure = Adventure.new(adventure_params)
+      print "this is the chosen adventure:"
+      print @adventure
+      if(@adventure.save)
+        redirect_to @adventure
+      else
+        render 'new'
+      end
+    end
+
+  def show
+    @adventure = Adventure.find(params[:id])
+  end
+
+
+  def edit
+      @adventure = Adventure.find(params[:id])
+    end
+
+  def update
+    @adventure = Adventure.find(params[:id])
+    if(@adventure.update(adventure_params))
+      redirect_to @adventure
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    print "--- delete adventure ---"
+    @adventure = Adventure.find(params[:id])
+    @adventure.destroy
+    redirect_to home_path
+  end
+
+  private def adventure_params
+    params.require(:adventure).permit(:title, :description, :creator)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'pages#index', as: 'home'
   resources :adventures
+
 end
