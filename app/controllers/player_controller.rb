7f28e575aa2ae5464f9d966b21782d226f958ecf
odellw <walker.odell@gmail.com>
2018-11-30 15:00:56 -0800

    afdas

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,5 +1,6 @@
 class PlayerController < ApplicationController
   def startadventure
     @adventure = Adventure.find(params[:adventure_id])
+    @scenes = Scene.where(adventure_id: @adventure.id)
   end
 end
